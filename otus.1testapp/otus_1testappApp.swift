c6e74cee7900b78a01a413d9762ab1d4e7a7ae56
Vladislav Ivshin <wenomok@gmail.com>
2022-01-08 16:27:53 +0400

    Initial Commit

---
+++ otus.1testapp/otus_1testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  otus_1testappApp.swift
+//  otus.1testapp
+//
+//  Created by Vladislav Ivshin on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct otus_1testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
